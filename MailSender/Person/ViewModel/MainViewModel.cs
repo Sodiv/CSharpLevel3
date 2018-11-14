aaca01696caa4bbb324a2e50b5e729f0bf24d656
Дмитрий Созонов <sodiv@mail.ru>
2018-11-14 16:55:51 +0500

    Импорт из csv

--- MailSender/Person/ViewModel/MainViewModel.cs
+++ MailSender/Person/ViewModel/MainViewModel.cs
@@ -1,9 +1,11 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
 using PersonLib;
 using PersonLib.Data;
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Input;
 
 namespace Person.ViewModel
@@ -27,6 +29,9 @@ namespace Person.ViewModel
         }
 
         public ICommand UpdateDataCommand { get; }
+        public ICommand AddDataCommand { get; }
+        public ICommand SaveDataCommand { get; }
+        public ICommand OpenFileCommand { get; }
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -35,6 +40,9 @@ namespace Person.ViewModel
             _DataAccessService = DataService;
 
             UpdateDataCommand = new RelayCommand(OnUpdateDataCommandExecuted, UpdateDataCommandCanExecute);
+            AddDataCommand = new RelayCommand(OnAddDataCommandExecuted);
+            SaveDataCommand = new RelayCommand(OnSaveDataCommandExecuted, SaveDataCommandCanExecute);
+            OpenFileCommand = new RelayCommand(OnOpenFileCommand);
         }
 
         private bool UpdateDataCommandCanExecute() => true;
@@ -43,6 +51,47 @@ namespace Person.ViewModel
         {
             Peoples = await _DataAccessService.GetPersonsAsync();
             RaisePropertyChanged(nameof(Peoples));
+        }
+
+        private void OnAddDataCommandExecuted()
+        {
+            SelectedPeople = new People();
+            Peoples.Add(SelectedPeople);
+        }
+
+        private bool SaveDataCommandCanExecute() => false;
+
+        private async void OnSaveDataCommandExecuted()
+        {
+            SelectedPeople.Id = await _DataAccessService.SavePersonAsync(SelectedPeople);
+        }
+
+        private async void OnOpenFileCommand()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            string filePath = "";
+            if (ofd.ShowDialog() == true)
+            {
+                filePath = ofd.FileName;
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] s = sr.ReadLine().Split(',');
+                        People temp = new People
+                        {
+                            FullName = s[0],
+                            Email = s[1],
+                            TelNumber = s[2]
+                        };
+                        if ((temp.Id = await _DataAccessService.SavePersonAsync(temp)) > 0)
+                        {
+                            Peoples.Add(temp);
+                        }
+                    }
+                }
+            }
+            else return;
         }
     }
 }
--- MailSender/PersonLib/IDataAccessService.cs
+++ MailSender/PersonLib/IDataAccessService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ namespace PersonLib
     {
         ObservableCollection<People> GetPersons();
         Task<ObservableCollection<People>> GetPersonsAsync();
+
+        Task<int> SavePersonAsync(People people);
     }
 
     public class DataAccessServiceFromDB : IDataService
@@ -27,6 +30,17 @@ namespace PersonLib
         {
             using (var db = new PeopleContext())
                 return new ObservableCollection<People>(await db.Peoples.ToArrayAsync());
+        }
+
+        public async Task<int> SavePersonAsync(People people)
+        {
+            using(var db=new PeopleContext())
+            {
+                db.Peoples.AddOrUpdate(people);
+                if (await db.SaveChangesAsync().ConfigureAwait(false) > 0)
+                    return people.Id;
+            }
+            return 0;
         }
     }
 }
